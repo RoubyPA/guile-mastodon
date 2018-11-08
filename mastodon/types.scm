5a10df38c50692a8010fc496500e1025397fb56d
Pierre-Antoine Rouby <contact@parouby.fr>
2018-11-08 14:50:25 +0100

    mastodon: Add account type.
    * mastodon/types.scm (<mastodon-account>): New record type.

--- mastodon/types.scm
+++ mastodon/types.scm
@@ -22,12 +22,69 @@
             instance?
             instance-name
             instance-url
-            instance-token))
+            instance-token
 
-;;; Define type
+            <mastodon-account>
+            account
+            account?
+            account-id
+            account-username
+            account-acct
+            account-display-name
+            account-locked
+            account-created-at
+            account-followers-counts
+            account-following-counts
+            account-statuses-counts
+            account-note
+            account-url
+            account-avatar
+            account-avatar-static
+            account-header
+            account-header-static
+            account-emojis
+            account-moved
+            account-fields
+            account-bot))
+
+;;;
+;;; Define types
+;;;
+
+;;; Instance
 (define-record-type <mastodon-instance>
   (instance name url token)
   instance?
   (name  instance-name)
   (url   instance-url)
   (token instance-token))
+
+;;; Account
+(define-record-type <mastodon-account>
+  (account id             username acct   display-name locked
+           created-at     followers-count following-count
+           statuses-count note            url
+           avatar         avatar-static   header
+           header-static  emojis          moved
+           fields         bot)
+  account?
+  ;; source: https://docs.joinmastodon.org/api/entities/#account
+  (id              account-id)               ;String
+  (username        account-username)         ;String
+  (acct            account-acct)             ;String
+  (display-name    account-display-name)     ;String
+  (locked          account-locked)           ;Boolean
+  (created-at      account-created-at)       ;String (Datetime)
+  (followers-count account-followers-counts) ;Number
+  (following-count account-following-counts) ;Number
+  (statuses-count  account-statuses-counts)  ;Number
+  (note            account-note)             ;String
+  (url             account-url)              ;String (URL)
+  (avatar          account-avatar)           ;String (URL)
+  (avatar-static   account-avatar-static)    ;String (URL)
+  (header          account-header)           ;String (URL)
+  (header-static   account-header-static)    ;String (URL)
+  (emojis          account-emojis)           ;List of Emoji
+  (moved           account-moved)            ;Account
+  (fields          account-fields)           ;List of Hash
+  (bot             account-bot))             ;Boolean
