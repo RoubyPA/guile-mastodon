eb97943274cb738ab7f7c2e9d40a5283a28946c0
Pierre-Antoine Rouby <contact@parouby.fr>
2018-11-13 18:18:51 +0100

    mastodon: Update dir-locals.
    * .dir-locals.el (scheme-mode): Add define-json-mapping in keyword-face.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,10 @@
 ((nil . ((fill-column . 78)
          (tab-width   .  8)))
- (scheme-mode . ((indent-tabs-mode . nil)))
+ (scheme-mode . ((indent-tabs-mode . nil)
+                 (eval . (setq scheme-font-lock-keywords-2
+                               (cons
+                                '("(\\(define-json-mapping\\)"
+                                  .
+                                  ((1 font-lock-keyword-face)))
+                                scheme-font-lock-keywords-2)))))
  (emacs-lisp-mode . ((indent-tabs-mode . nil))))
